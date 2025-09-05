7b9cfb1507d124658823deee7fcd39c472d18607
Pedro Flores <manuelflores1795@gmail.com>
2025-09-04 20:00:32 -0500

    fix: deploy and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://flobell.github.io/portfolio/',
-  base: '/portfolio/', // <- Muy importante para GitHub Pages
+  site: 'https://flobell.github.io',
+  base: '/portfolio',
 });
